a4e58977ee64d91a867013c1d56e0ffc1cec59f0
varta5 <97350993+varta5@users.noreply.github.com>
2022-02-08 21:25:33 +0100

    Styles added, dummy values in HTML
    Added some predifend demmy values (temporarily) to the HTML code. Added most CSS in a new CSS file (style.css). Did not alter the JavaScript much, that needs to be tailored for the new HTML structure (table instead of UL)

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 let i = 0;
 
+/* THIS IS THE VERSION WHEN LI-S WERE ADDED TO THE UL
+
 function itemDone(itemIndex) {
     $('#listItem' + itemIndex).on('click', function () {
         $('#listItem' + itemIndex).css('text-decoration', 'line-through');
@@ -9,6 +11,22 @@ function itemDone(itemIndex) {
 
 $("#addButton").on("click", function () {
     $("ul").append('<li id=listItem' + i + '>' + $('#textToDo').val() + '</li>');
+    console.log("One item added to the To Do list. Index of list item: " + i);
+    itemDone(i);
+    i = i + 1;
+})
+
+*/
+
+function itemDone(itemIndex) {
+    $('#listItem' + itemIndex).on('click', function () {
+        $('#listItem' + itemIndex).css('text-decoration', 'line-through');
+        console.log('ToDo list item number ' + itemIndex + ', (' + $('#listItem' + itemIndex).text() + ') has been marked as completed.');
+    });
+}
+
+$("#addButton").on("click", function () {
+    $(".theList").append('<div id=listItem' + i + '>' + $('#textToDo').val() + '</div>');
     console.log("One item added to the To Do list. Index of list item: " + i);
     itemDone(i);
     i = i + 1;
